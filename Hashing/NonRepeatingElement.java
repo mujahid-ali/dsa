24f6f8bd431d924fa1178c6f867dc3b7b7fb263e
mujahid-ali <mujahidali0611@gmail.com>
2019-07-29 22:00:09 +0530

    WIP: NonRepeatingElement

--- Hashing/NonRepeatingElement.java
+++ Hashing/NonRepeatingElement.java
@@ -6,17 +6,17 @@ import java.lang.*;
 class GFG{
 
 	public static boolean helper(long arr[], int n){
-		HashMap<Long,Long> hm = new <Long,Long>();
+		HashMap<String, Integer> hm = new HashMap<String,Integer>();
 		int i;
 		boolean flag = false;
 		for(i=0;i<n;i++){
 			if(hm.containsKey(arr[i])){
-				hm.put(arr[i],hm.get(arr[i])+1)
+				hm.put(Long.toString(arr[i]),hm.get(arr[i])+1);
 			}else
-				hm.put(arr[i],1);
+				{hm.put(Long.toString(arr[i]),1);}
 		}
 		for(i=0;i<n;i++){
-			if(hm.get(arr[i]==1)){
+			if(hm.get(arr[i]) ==1){
 				flag = true;
 				System.out.println(arr[i]);
 				break;
@@ -26,11 +26,11 @@ class GFG{
 	}
 	public static void main(String[] args)throws java.lang.Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Interger.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
 
 		while(t-->0){
 			int n = Integer.parseInt(br.readLine());
-			String str[] = br.readLine().split();
+			String str[] = br.readLine().split(" ");
 			int i =0;
 			long arr[] = new long[n];
 			for(i=0;i<n;i++){
