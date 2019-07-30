ba288f7f6ead73de5f8d267f0699fbbcace71e2a
mujahidali <mujahidali0611@gmail.com>
2019-07-30 09:06:53 +0530

    WIP: Most Frequent Word

--- hashing/Mostfrequentword.java
+++ hashing/Mostfrequentword.java
@@ -15,13 +15,29 @@ class GFG{
 				HM.put(str[i],1);
 				}	
 			}
+		String most = "";	
 		for(i=0;i<n;i++){
-			if(HM.get(str[i])>= max){
+			if(HM.get(str[i])> max){
 				max = HM.get(str[i]);
 				count = i;
+				most = str[i];
 				}
+			else{
+			    if(HM.get(str[i]) == max){
+			        max = HM.get(str[i]);
+			        count = i;
+			    }
+			}	
 			}
-		System.out.println(str[count]);		
+		//System.out.println("most is  "+most);	
+		System.out.println(str[count]);
+		
+		for(String s: HM.keySet()){
+			String key = s;
+			String value = HM.get(s).toString();
+			//System.out.println(key + ": " + value);
+			}
+				
 		}
 	
 	public static void main(String[] args)throws java.lang.Exception{
