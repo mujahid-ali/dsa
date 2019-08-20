39f54baf2a78b06fa2eb914ff4b50752cb29f0d0
mujahid-ali <mujahidali0611@gmail.com>
2019-08-20 22:40:33 +0530

    Remove Duplicates form unsoreted LL

---
+++ linked-list/RemoveDuplicatesUnsorted.java
@@ -0,0 +1,26 @@
+class GfG
+{
+    // Function to remove duplicates from the given linked list
+    Node removeDuplicates(Node head)
+    {
+         // Your code here
+         //int arr[] = new int[50];
+         List<Integer> arr = new ArrayList<>();
+         Node temp = head.next;
+         Node prev = head;
+         int i = 0;
+         while(temp != null){
+             if(arr.contains(temp.data)){
+                 prev.next = temp.next;
+                 temp = temp.next;
+             }
+             else{
+                 //arr[i++] = temp.data;
+                 arr.add(temp.data);
+                 prev = temp;
+                 temp = temp.next;
+             }
+         }
+         return head;
+    }
+}
