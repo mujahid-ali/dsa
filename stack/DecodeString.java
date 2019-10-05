95f7440fc725eef67db32bc189e503a4a8b31289
mujahid-ali <mujahidali0611@gmail.com>
2019-10-05 21:14:17 +0530

    Decode String

--- stack/DecodeString.java
+++ stack/DecodeString.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 class GFG{
   public static void main(String[] args)throws java.lang.Exception{
-  Bufferedreader br = new BufferedReader(new InputStreamReader(System.in));
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   int t = Integer.parseInt(br.readLine().trim());
 
   while(t-->0){
@@ -17,37 +17,52 @@ class GFG{
 
   static void decode_string(String str){
     int n = str.length();
-    Stack<Character> stack = new Stack<Character>();
-    StringBuilder result = "";
+    //Stack<Character> stack = new Stack<Character>();
+    Stack<String> stack = new Stack<String>();
+    //StringBuilder result = new StringBuilder();
+    String result = "";
 
     for(int i =0; i<n; i++){
       char c = str.charAt(i);
 
       if( (Character.isDigit(c)) || (c == '[') || Character.isLetter(c) ){
-        stack.push(c);
+        stack.push(Character.toString(c));
       }
       else if( c == ']' ){
         //pop until digit.
-        StringBuilder sb = "";
-        while(!Character.isDigit(stack.peek())) {
-          char c1 = stack.pop();
+        //StringBuilder sb = new StringBuilder();
+        String sb = "";
+        while(!Character.isDigit(stack.peek().charAt(0))) {
+          String temp = stack.pop();
+          //System.out.println("temp is = "+ temp);
+          char c1 = temp.charAt(0);
           if(Character.isLetter(c1)){
-            sb = c1 + sb;
+            //sb = Character.toString(c1) + sb;
+            sb = temp + sb;
           }
         }
-        char c2 = stack.pop();
-        int x = Integer.parseInt(c2);
+
+        String temp_dig = "";
+        while(!stack.isEmpty() && (Character.isDigit(stack.peek().charAt(0)))){
+          temp_dig =  stack.pop() + temp_dig;
+          //System.out.println("temp dig = "+temp_dig);
+        }
+        //String c2 = stack.pop();
+        //int x = Integer.parseInt(Character.toString(c2));
+        int x = Integer.parseInt(temp_dig);
         result = expand(sb,x);
         stack.push(result);
       }
     }
+    System.out.println(result);
   }
 
   static String expand(String str, int x){
+    String result = str;
 
     while(--x>0){
-        str += str;
+        result = result + str;
     }
-    return str;
+    return result;
   }
 }
