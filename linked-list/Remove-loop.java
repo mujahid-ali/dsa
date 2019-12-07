26b8e8f420ee3c01878742cbe976cec10342db3c
mujahid-ali <mujahidali0611@gmail.com>
2019-12-07 08:21:30 +0530

    Remove loop from ll using hashing

--- linked-list/Remove-loop.java
+++ linked-list/Remove-loop.java
@@ -1,7 +1,31 @@
 class Loop{
 
-    // This function will remove the loop from the linked list
+    //hashing
     public static void removeTheLoop(Node head)
+    {
+        //Your code here
+        if(head == null)
+            return;
+        HashSet<Node> hs = new HashSet<Node>();
+        Node prev = null;
+        Node temp = head;
+
+        while(temp != null){
+            if(hs.contains(temp)){
+                prev.next = null;
+                break;
+            }
+            else{
+                hs.add(temp);
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+    }
+
+    // This function will remove the loop from the linked list
+    //Using slow and fast ptr
+    public static void removeTheLoop2(Node head)
     {
         //Your code here
         if(head == null || head.next == null)
@@ -27,3 +51,34 @@ class Loop{
         }
     }
 }
+
+
+/*
+CPP
+void removeTheLoop(Node *head)
+{
+     //Your code here
+     if(head == NULL || head->next == NULL)
+            return;
+
+        Node *slow = head;
+        Node *fast = head->next->next;
+
+        while( fast != NULL && fast->next != NULL){
+            if(slow == fast)
+                break;
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        if(slow == fast){
+            slow = head;
+            while(slow->next != fast->next){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            fast->next = NULL;
+        }
+}
+
+*/
